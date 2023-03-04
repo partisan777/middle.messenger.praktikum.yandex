226e63e94bc5dd480b6ac114de1304c2b957ee5a
Nikita <n173@bk.ru>
2023-03-04 03:26:28 +0300

    save 04-03  2

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import  {isAuth} from "./utils/auth";
+
+import { addMainWindowFull } from "./mainFIles/addMainWindow"
+import { addRegFormFull } from "./mainFIles/addRegForm";
+
+
+const isAuthState: boolean = isAuth();
+console.log(isAuth());
+if (!isAuthState) {
+    addRegFormFull();
+} else {
+    addMainWindowFull();
+}
+
+
+
---
+++ src/mainFIles/addRegForm.ts
@@ -0,0 +1,54 @@
+
+import { RegFormPage } from "../pages/regForm/regForm"
+import { LoginFormPage } from "../pages/regForm/loginForm"
+import {logIn, isAuth} from '../utils/auth';
+
+export function addRegFormFull(): void {
+
+    const container = document.getElementById("root");
+
+    let regForm = new RegFormPage({
+        pageTitle: 'Регистрация',
+        regHasAccButtonEvents: {events:
+            {click: (e: Event) => {
+                    regForm.changeVisible()
+                    loginForm.changeVisible()
+                    e.preventDefault();
+                }
+            }
+        },
+        registrationButtonEvents: {events:
+            {click: (e: Event) => {
+                   e.preventDefault();
+                }
+            }
+        }
+    });
+
+
+    regForm.hide()
+    let loginForm = new LoginFormPage({
+        pageTitle: 'Войти',
+        logHasAccButtonEvent:
+            {events:
+                {click: (e: Event) => {
+                        regForm.changeVisible()
+                        loginForm.changeVisible()
+                        e.preventDefault();
+                    }
+                }       
+        },
+        loginButtonEvents: {events:
+            {click: (e: Event) => {
+                    container.innerHTML = '';
+                    logIn();
+                    location.reload();
+                    e.preventDefault();
+                }
+            }       
+        }
+    });
+              
+    container.append(loginForm.getContent())
+    container.append(regForm.getContent())
+}
